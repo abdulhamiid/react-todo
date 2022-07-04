2277ad0eddc97c6cffef55fe9b5bae8373a79290
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-04 15:21:33 +0100

    Add component to handle the todos's array

---
+++ src/components/TodosList.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import React from 'react';
+import TodoItem from './TodoItem';
+class TodosList extends React.Component {
+  render() {
+    const { todos } = this.props;
+    const { handleChangeProps } = this.props;
+    return (
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} handleChangeProps={handleChangeProps} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodosList;

303496c33b57b9337a20c833cc0f4e1919a29211
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-05 14:23:35 +0100

    Update the inputTodo using hooks

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,25 +1,23 @@
 /* eslint-disable */
-import React, { Component } from "react"
+import React, { useState } from "react"
 
-class InputTodo extends Component {
-  constructor(props){
-    super (props)
-    this.state = {
-      title: ""
-    };
-  }
+const InputTodo = props => {
+  const [inputText, setInputText] = useState({
+    title: "",
+  })
 
-  onChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
+  const onChange = e => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
     })
   }
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault()
-    if (this.state.title.trim()) {
-      this.props.addTodoProps(this.state.title)
-      this.setState({
+    if (inputText.title.trim()) {
+      props.addTodoProps(inputText.title)
+      setInputText({
         title: "",
       })
     } else {
@@ -27,19 +25,19 @@ class InputTodo extends Component {
     }
   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input 
-          type="text" 
-          className="input-text"
-          placeholder="Add Todo..." 
-          value={this.state.title} 
-          name="title"
-          onChange={this.onChange} />
-        <button className="input-submit">Submit</button>
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={inputText.title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
+  )
 }
+
 export default InputTodo
